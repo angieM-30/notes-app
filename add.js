7f59ff41f585a14891cc519a5c9d29fbe1b83c31
madronaangelica30@gmail.com <madronaangelica30@gmail.com>
2022-10-18 10:32:37 +0800

    read.js and write.js added

--- add.js
+++ add.js
@@ -1,11 +1,10 @@
 const fs = require('fs')
 
 const add = function(note, oldNote) {
-
-let newNote = JSON.parse(oldNote);
+const newNote = JSON.parse(oldNote);
+// add newNote to oldNote
 
 newNote.push(note);
-  // console.log(newNote);
 
   fs.writeFileSync('./Note.txt', JSON.stringify(newNote));
 }
